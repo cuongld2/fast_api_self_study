adbd1eb4c9399a9e609c5607ad5bb98d92652894
cuongld2 <ledinhcuong99@gmail.com>
2020-03-15 11:21:00 +0700

    base64 encode password

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-
+import base64
 from . import models, schemas
 
 
@@ -8,8 +8,8 @@ def get_user_by_username(db: Session, username: str):
 
 
 def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed_password = user.password + "notreallyhashed"
-    db_user = models.UserInfo(username=user.username, password=fake_hashed_password, fullname=user.fullname)
+    hashed_password = base64.b64encode(user.password.encode('utf-8'))
+    db_user = models.UserInfo(username=user.username, password=hashed_password, fullname=user.fullname)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
